830b37ef357cf25b5e8826ce759cc3c7c0e150ad
pandacyy <pandacyy1@gmail.com>
2017-11-08 10:17:04 +0900

    python
    Signed-off-by: pandacyy <pandacyy1@gmail.com>

---
+++ mysite/elections/tests.py
@@ -0,0 +1,8 @@
+#My Development\Python\Workspace\Django\mysite
+#python manage.py runserver
+#python manage.py makemigrations
+#python manage.py migrate
+
+from django.test import TestCase
+
+# Create your tests here.
---
+++ mysite/elections/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^areas/(?P<area>[가-힣]+)/$', views.areas),
+    url(r'^areas/(?P<area>[가-힣]+)/results$', views.results),
+    url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
+]
